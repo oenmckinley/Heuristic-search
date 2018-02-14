e6655aa4882e3390e0d053ac5fa13e38b89162f9
mckinley1 <mckinley1@grinnell.edu>
2018-02-13 18:50:24 -0600

    done

--- astar/driver.scm
+++ astar/driver.scm
@@ -8,7 +8,8 @@
 ;;   A collection of tests for our A* search.
 ;;
 ;; Provides
-;;   (test-astar)
+;;   (test-astar-main)
+;;   (test-astar-best)
 ;;   (run-astar-main-test values)
 ;;   (run-astar-best-test values)
 ;;   (run-ids-test values)
@@ -27,47 +28,72 @@
 (load "astar.scm")
 
 
-
-;; Sample test, run test on multiple lists of varying length and make sure that
-;; they return the right values.
-
-(define test-astar
+;; TEST MAIN HEURISTIC ON VALUES (1 10 100 400)
+(define test-astar-main
   (test-suite
    "Tests of astar"
-   (test-case
-    "small problems"
+    (test-case
+    "smallest problem"
+    (let ([sol (astar-search (jump-start-state 1) (jump-problem 1) jump-heuristic-main)])
+      (check-equal?  sol
+                     '((1) 1)
+                     "works on smallest problem")))(test-case
+    "small problem"
     (let ([sol (astar-search (jump-start-state 10) (jump-problem 10) jump-heuristic-main)])
-      (check-equal? (equal? #f sol)
-                     #f
-                     "works on smallest problem")))))
+      (check-equal?  sol
+                     '((1 3 3 2 1) 13)
+                     "works on smallest problem")))
+      (test-case
+    "medium problem"
+    (let ([sol (astar-search (jump-start-state 100) (jump-problem 100) jump-heuristic-main)])
+      (check-equal?  sol
+                     '((1 3 5 7 9 11 10 9 9 8 7 6 5 4 3 2 1) 4413)
+                     "works on medium problem")))
+         (test-case
+    "long problem"
+    (let ([sol (astar-search (jump-start-state 400) (jump-problem 400) jump-heuristic-main)])
+      (check-equal?  sol
+                     '((1 3 5 7 9 11 13 15 17 19 21 22 21 20 19 18 17 17 16 15 14 13 12 11
+                        10 9 9 8 7 6 5 4 3 2 1) 6101157)
+                     "works on large problem")))))
 
-(run-tests test-astar)
+;; TEST MAIN HEURISTIC ON VALUES (1 10 100 400)
+(define test-astar-best
+  (test-suite
+   "Tests of astar"
+    (test-case
+    "smallest problem"
+    (let ([sol (astar-search (jump-start-state 1) (jump-problem 1) jump-heuristic-best)])
+      (check-equal?  sol
+                     '((1) 1)
+                     "works on smallest problem")))(test-case
+    "small problem"
+    (let ([sol (astar-search (jump-start-state 10) (jump-problem 10) jump-heuristic-best)])
+      (check-equal?  sol
+                     '((1 3 2 2 1 1) 6)
+                     "works on smallest problem")))
+      (test-case
+    "medium problem"
+    (let ([sol (astar-search (jump-start-state 100) (jump-problem 100) jump-heuristic-best)])
+      (check-equal?  sol
+                     '((1 3 5 7 9 11 10 9 8 7 6 6 5 4 3 2 2 1 1) 19)
+                     "works on medium problem")))
+         (test-case
+    "long problem"
+    (let ([sol (astar-search (jump-start-state 400) (jump-problem 400) jump-heuristic-best)])
+      (check-equal?  sol
+                     '((1 3 5 7 9 11 13 15 17 19 21 22 21 20 19 18 17 17 16 15 14 13 12 11 10 9 8 7
+                        6 6 5 4 3 2 2 1 1) 37)
+                     "works on large problem")))))
 
-;; list of values increasing by a power of 10 from 10 to 1,000,000,000
-(define vals (list 10 100 1000 10000 100000 1000000 10000000 100000000 1000000000))
+;; RUN TESTS
+(display "Testing Main A* Heuristic . . .") (newline)
+(run-tests test-astar-main) (newline)
+(display "Testing Best A* Heuristic . . .") (newline)
+(run-tests test-astar-best) (newline)
 
-;; function definitions 
-(define run-astar-main-test
-  (lambda(values)
-    (display "hi") ;; print result of running search on car values 
-    (if (equal? values '())
-        "Done"
-        (run-astar-main-test (cdr values)))))
 
-(define run-astar-best-test
-  (lambda(values)
-    (display "hi") ;; print result of running search on car values 
-    (if (equal? values '())
-        "Done"
-        (run-astar-main-test (cdr values)))))
-
-(define run-ids-test
-  (lambda(values)
-    (display "hi") ;; print result of running search on car values 
-    (if (equal? values '())
-        "Done"
-        (run-astar-main-test (cdr values)))))
-
+;; CODE FOR SIDE BY SIDE DATA COLLECTION/COMPARISON
 (define side-by-side
   (lambda (n)
     (display "Comparison of IDS and A* with n = ")
